aebc15afe1f0eac996396fd68892b0edb39fb9b3
DebiprasadXD <debiprasaddaskdv@gmail.com>
2023-02-21 10:01:24 +0530

    show all user notes and delete button is working as well

--- src/App.js
+++ src/App.js
@@ -1,12 +1,105 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Login from './components/login';
 import Notes from './components/notes'
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import About from './components/about';
 import Header from './components/header';
+import axios from 'axios'
+import Cookies from 'js-cookie';
 
 function App(){
+  let accessToken = Cookies.get('AccessToken')
+  let refreshToken = Cookies.get('RefreshToken')
+  let navigate = useNavigate()
 
+  let [notes, setNotes] = useState("")
+  const [loading, setLoading] = useState(true);
+  const [counter, setCounter] = useState(0);
+
+  const isAuthenticated = () =>{
+      if (accessToken | refreshToken === undefined){
+          navigate('/login')
+          console.log("no auth")
+      }
+  }
+  const getNotes = async() =>{
+    let token = accessToken
+    let config = {
+        headers: {
+        'Authorization' : 'Bearer ' + token
+        }}
+    try{
+        console.log('processing')
+        let response = await axios.get('https://notemy-api.deta.dev/api/v1/notes/',config)
+
+        let data = response.data
+
+
+        let allNotes = []
+
+
+        let firstPageNotes = data.data
+        for(let i =0; i<firstPageNotes.length; i++){
+          allNotes.push(firstPageNotes[i])
+        }
+        
+        let meta = data.meta
+
+        let pages = meta['pages']
+
+        for (let page=2; page<pages+1; page++){
+          let response = await axios.get('https://notemy-api.deta.dev/api/v1/notes/?page=' + page, config)
+          let data = response.data
+          let notes = data.data
+          //console.log(notes)
+          for (let i=0; i<5; i++){
+            if (notes[i] !== undefined){
+              allNotes.push(notes[i])
+            }
+           
+            
+            
+           }
+
+          
+
+
+         }
+
+        setNotes(allNotes)
+        console.log(allNotes)
+        setLoading(false)
+        
+        // increase the counter
+        setCounter((oldValue) => oldValue+1);
+    }
+    catch{
+        navigate('/login')
+
+    }
+}
+  const Delete = async(id) => {
+    let token = accessToken
+            let config = {
+                headers: {
+                'Authorization' : 'Bearer ' + token
+                }}
+            try{
+                console.log('processing delete')
+                setLoading(true)
+                let response = await axios.delete("https://notemy-api.deta.dev/api/v1/notes/" + id  + '/', config)
+                //console.log(response)
+                if (response.status === 204){
+                  await getNotes()
+                  setLoading(false)
+                }
+                //alert('your note is deleted')
+                
+              }
+            catch{
+              alert("Failed to delete your note")
+            }
+}
   
   return(
     <div>
@@ -18,7 +111,7 @@ function App(){
           element={
             <>
             
-            <Notes />
+            <Notes isAuthenticated={isAuthenticated} Delete={Delete} getNotes={getNotes} notes={notes} loading={loading}/>
             
             </>
           }
--- src/components/notes.jsx
+++ src/components/notes.jsx
@@ -1,82 +1,51 @@
-import { useState, React, useEffect} from "react"
-import Cookies from 'js-cookie'
-import { useNavigate } from "react-router-dom";
-import axios from "axios";
+import { React, useEffect} from "react"
 import { useRef } from "react";
 import NoteItem from "./noteItem";
 
 
-const Notes = () => {
-    let accessToken = Cookies.get('AccessToken')
-    let refreshToken = Cookies.get('RefreshToken')
-    let navigate = useNavigate()
-
-    let [notes, setNotes] = useState("")
-    const isAuthenticated = () =>{
-        if (accessToken | refreshToken === undefined){
-            navigate('/login')
-            console.log("no auth")
-        }
-    }
-    const [counter, setCounter] = useState(0);
+const Notes = (props) => {
     const dataFetchedRef = useRef(false);
     
+
     useEffect(() =>{
-        // don't run useEffect twice
+        //don't run useEffect twice
         if (dataFetchedRef.current) return;
         dataFetchedRef.current = true;
-
-        isAuthenticated()
-        getNotes()      
-        },[])
-
-
-        const getNotes = async() =>{
-            let token = accessToken
-            let config = {
-                headers: {
-                'Authorization' : 'Bearer ' + token
-                }}
-            try{
-                console.log('processing')
-                let response = await axios.get('https://notemy-api.deta.dev/api/v1/notes/',config)
-               
-                let data = response.data
-                let notes = data.data
-                console.log(notes)
-                setNotes(notes)
-                // increase the counter
-                setCounter((oldValue) => oldValue+1);
-            }
-            catch{
-                navigate('/login')
-    
-            }
-        }
-    
-    
-    return (
         
-        <>
-
-        <div className="container">
-            <h2 className="text-center">Your Notes</h2>
-            <ul>
-            {notes.length===0  ? <p>NO TODOS</p> : (
-      notes.map((note) => {
-        return (
-            <NoteItem note={note} key={note.id} />
-        );
-      }))
-      }
-            </ul>
+        props.isAuthenticated()
+        props.getNotes()
+        
+              
+        },[props])
+    return (
+        <div className="container ">
+            <h2 className="d-flex justify-content-center">NOTES</h2>
+            {props.loading === false ?
+            (<ul className="list-group my-4">
+            {props.notes.length===0  ?
+             (<p>NO TODOS</p>) : 
+            props.notes.map((note) => 
+                {
+                return(
+                <NoteItem note={note} key={note.id} Delete={props.Delete}/>
+                )}
+                                )}
+            </ul>)
+      : (
+        <div className="text-center mt-5">
+  <div className="spinner-grow" role="status">
+    <span className="visually-hidden">Loading...</span>
+  </div>
+</div>
+     )
+    }
+           
             
             
 
             
 
         </div>
-        </>
     )
 }
 
